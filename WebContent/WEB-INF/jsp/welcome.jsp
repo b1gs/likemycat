4a9a463a88023178c672d79288ad594810270168
jenkins <b1gsmen@gmail.com>
2016-09-08 16:40:32 +0300

    First commit

---
+++ WebContent/WEB-INF/jsp/welcome.jsp
@@ -0,0 +1,9 @@
+<html>
+<head><title>First JSP</title></head>
+<body>
+  
+  Hello nigga!!! 
+  
+  <a href="<%= request.getRequestURI() %>"><h3>Try Again</h3></a>
+</body>
+</html>
---
+++ src/net/likemycat/controller/BasicController.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package net.likemycat.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * @author oleksandr.volkovskyi
+ *
+ */
+@Controller
+public class BasicController {
+
+	
+	
+	@RequestMapping("/welcome")
+	public ModelAndView welcomePage(){
+		
+		ModelAndView model = new ModelAndView();
+		model.setViewName("welcome");
+		return model;
+		
+	}
+	
+	@RequestMapping({"/index","/"})
+	public ModelAndView indexPage(){
+		
+		ModelAndView model = new ModelAndView();
+		model.setViewName("index");
+		return model;
+		
+	}
+	
+}
